8175e77e4b442ede351f05931054c48539d3c838
Leonardo7Silva <leo7kisam@gmail.com>
2022-07-20 11:32:49 -0300

    html/css tela de boas vindas

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom";
+import TelaInicial from "./telaInicial";
+
+function  App(){
+return (
+    <>
+        <TelaInicial/>
+    </>
+);
+}
+
+ReactDOM.render(<App/>, document.querySelector(".root"));
---
+++ src/telaInicial.js
@@ -0,0 +1,13 @@
+function TelaInicial(){
+    return(
+        <div className="primeiraTela">
+            <img src="assets/logo.png" />
+            <h1>ZapRecall</h1>
+            <div>
+                <h2>Iniciar Recall!</h2>
+            </div>
+        </div>
+    );
+}
+
+export default TelaInicial
